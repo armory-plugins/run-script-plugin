1018fd34d737244b66a97d2017a2a74ae7c1e028
Ethan Rogers <ethanfrogers@gmail.com>
2020-08-11 09:45:34 -0400

    feat(config): allow artifact service override
    allow artifact service and git repo account to be overridden. refactor
    plugin instantiation to be more testable by extracting it into a
    separate class.

---
+++ run-script-plugin-orca/src/main/kotlin/com/armory/plugin/preconfigured/ConfigOverrider.kt
@@ -0,0 +1,46 @@
+package com.armory.plugin.preconfigured
+
+import com.netflix.spinnaker.orca.clouddriver.config.KubernetesPreconfiguredJobProperties
+import io.kubernetes.client.models.V1EnvVar
+import java.lang.IllegalStateException
+
+class ConfigOverrider() {
+  companion object {
+    /**
+     * swap manifest properties for configuration values
+     */
+    fun override(configuration: PluginConfig, jobProperties : KubernetesPreconfiguredJobProperties) {
+      if (!configuration.account.isNullOrEmpty()) {
+        jobProperties.account = configuration.account
+      }
+      if (!configuration.credentials.isNullOrEmpty()) {
+        jobProperties.credentials = configuration.credentials
+      }
+
+      val initContainer = jobProperties.manifest.spec.template.spec.initContainers.firstOrNull{ it.name == "git" }
+
+      if (initContainer == null) {
+        throw IllegalStateException("job manifest must have initContianer with name git")
+      }
+
+      if (!configuration.initContainerImage.isNullOrEmpty()) {
+        initContainer.image = configuration.initContainerImage
+      }
+
+      // TODO: there's probably a way more efficient way to do this.
+      if (!configuration.artifactServiceUrl.isNullOrEmpty()) {
+        val targetEnvVariable = initContainer.env.firstOrNull{ it.name == "ARTIFACT_SERVICE" }
+        if (targetEnvVariable != null) {
+          targetEnvVariable.value = configuration.artifactServiceUrl
+        }
+      }
+
+      if (!configuration.gitArtifactAccount.isNullOrEmpty()) {
+        val targetEnvVariable = initContainer.env.firstOrNull{ it.name == "ARTIFACT_ACCOUNT" }
+        if (targetEnvVariable != null) {
+          targetEnvVariable.value = configuration.gitArtifactAccount
+        }
+      }
+    }
+  }
+}
